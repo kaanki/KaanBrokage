22e098404f65d6c8a80ecbd1427db26c805a0605
Kaan <kaan.keles@mail.com>
2025-08-31 01:33:44 +0300

    First Commit

---
+++ src/main/java/com/example/KaanBrokage/KaanBrokageApplication.java
@@ -0,0 +1,13 @@
+package com.example.KaanBrokage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaanBrokageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KaanBrokageApplication.class, args);
+	}
+
+}
